c52dbb2e735a4afc45c8b234a2e7677a5af49028
Dennis Courtney <52701185+DennisCourtney@users.noreply.github.com>
2021-11-13 09:25:45 -0500

    Updated

--- Drivetrain Code.py
+++ Drivetrain Code.py
@@ -6,7 +6,7 @@ import pygame
 from adafruit_servokit import ServoKit
 pygame.init()
 
-
+#ATTENTION: Text inside of a "#" are comments. This allows the author of the program to communicate with the reader.
 
 pwm = ServoKit(channels=16)
 leftstick = 0.07
@@ -28,15 +28,21 @@ while True:
         elif abs(gamepad.get_axis(4)) <= 0.1:
             rightstick = 0.07
             
-        
+       #For both of these axis values, you can get them from running the map_joystick.py program. 
+       #Most controllers will automatically use axis 1 and axis 4 naturally for a tankdrive configuration.
         leftstick = gamepad.get_axis(1)
         rightstick = gamepad.get_axis(4)
         
 
-    
+    # The values [0] and [1] reference which channel on the servo bonnet the motor controller signal and ground wires go.
+
         pwm.continuous_servo[0].throttle = leftstick
-        pwm.continuous_servo[7].throttle = rightstick
-               
+        pwm.continuous_servo[1].throttle = rightstick
+         
+            
+    # Print statements help us identify where to troubleshoot. These print statements show us the values
+    # that are being output by the motors and the input of the joystick values.
+    
         print("rightstick: ", rightstick)
         
         print("leftstick: ", leftstick)
